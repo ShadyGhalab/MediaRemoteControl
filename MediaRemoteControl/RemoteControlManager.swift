9e1227d5304084b373e9cb8f64db00fd8251cdd1
Shady Ghalab <shadyghalab@gmail.com>
2017-03-18 21:30:04 +0100

    Updating the copy rights

--- MediaRemoteControl/RemoteControlManager.swift
+++ MediaRemoteControl/RemoteControlManager.swift
@@ -1,6 +1,6 @@
 /* The MIT License (MIT)
  *
- * Copyright (c) 2017 Shady Ghalab (ktustanowski)
+ * Copyright (c) 2017 Shady Ghalab (shadyghalab)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
